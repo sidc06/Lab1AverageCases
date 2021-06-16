1cb12cf8a5f656b705ca7a21a82e999163674e54
Siddharth Choudhury <siddharth.choudhury@dcmail.ca>
2021-06-16 20:12:38 +0530

    Add project files.

---
+++ AverageCases.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace AverageCases
+{
+    partial class formAverageCases
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelDailyCasePrompt = new System.Windows.Forms.Label();
+            this.textBoxCaseEntry = new System.Windows.Forms.TextBox();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.textBoxCaseList = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // labelDailyCasePrompt
+            // 
+            this.labelDailyCasePrompt.Location = new System.Drawing.Point(24, 24);
+            this.labelDailyCasePrompt.Name = "labelDailyCasePrompt";
+            this.labelDailyCasePrompt.Size = new System.Drawing.Size(100, 25);
+            this.labelDailyCasePrompt.TabIndex = 0;
+            this.labelDailyCasePrompt.Text = "Daily &Cases: ";
+            this.labelDailyCasePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxCaseEntry
+            // 
+            this.textBoxCaseEntry.Location = new System.Drawing.Point(122, 22);
+            this.textBoxCaseEntry.Name = "textBoxCaseEntry";
+            this.textBoxCaseEntry.Size = new System.Drawing.Size(119, 27);
+            this.textBoxCaseEntry.TabIndex = 1;
+            this.toolTip.SetToolTip(this.textBoxCaseEntry, "Enter the number of confirmed cases for the current day");
+            // 
+            // labelDay
+            // 
+            this.labelDay.Location = new System.Drawing.Point(370, 27);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(100, 20);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "Day 1";
+            this.labelDay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelDay.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxCaseList
+            // 
+            this.textBoxCaseList.Location = new System.Drawing.Point(122, 67);
+            this.textBoxCaseList.Multiline = true;
+            this.textBoxCaseList.Name = "textBoxCaseList";
+            this.textBoxCaseList.ReadOnly = true;
+            this.textBoxCaseList.Size = new System.Drawing.Size(239, 236);
+            this.textBoxCaseList.TabIndex = 3;
+            this.textBoxCaseList.TabStop = false;
+            this.toolTip.SetToolTip(this.textBoxCaseList, "Displays the number of cases for each day of this week");
+            this.textBoxCaseList.TextChanged += new System.EventHandler(this.textBoxCaseList_TextChanged);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelOutput.Location = new System.Drawing.Point(18, 322);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(446, 31);
+            this.labelOutput.TabIndex = 4;
+            this.labelOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelOutput.Click += new System.EventHandler(this.labelOutput_Click);
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(24, 374);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(94, 29);
+            this.buttonEnter.TabIndex = 5;
+            this.buttonEnter.Text = "&Enter";
+            this.toolTip.SetToolTip(this.buttonEnter, "Click to enter the daily number of cases");
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.EnterClick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(195, 374);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(94, 29);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "&Reset";
+            this.toolTip.SetToolTip(this.buttonReset, "Click to reset the form");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ResetClick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(376, 374);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(94, 29);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "E&xit";
+            this.toolTip.SetToolTip(this.buttonExit, "Click to exit the application");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ExitClick);
+            // 
+            // formAverageCases
+            // 
+            this.AcceptButton = this.buttonEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonReset;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.textBoxCaseList);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.textBoxCaseEntry);
+            this.Controls.Add(this.labelDailyCasePrompt);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "formAverageCases";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Weekly Cases";
+            this.Load += new System.EventHandler(this.formAverageCases_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDailyCasePrompt;
+        private System.Windows.Forms.TextBox textBoxCaseEntry;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.TextBox textBoxCaseList;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ AverageCases.cs
@@ -0,0 +1,151 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// File Name: AverageCases.cs
+/// Author Name: Siddharth Choudhury
+/// Date: May 23, 2021
+/// Description: A form that takes user input for daily cases for seven days of the week and shows average weekly cases.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageCases
+{
+    public partial class formAverageCases : Form
+    {
+        // Declare class-level variables.
+        int currentDay = 1;
+        int totalCases = 0;
+
+        public formAverageCases()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxCaseList_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// This exits the application.
+        /// </summary>
+       
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+        /// <summary>
+        /// Clears all the fields and resets the form to its default state.
+        /// </summary>
+       
+        private void ResetClick(object sender, EventArgs e)
+        {
+            // Clear input and output controls
+            textBoxCaseEntry.Clear();
+            textBoxCaseList.Clear();
+            labelOutput.Text = String.Empty;
+
+            // Re-enable all controls which could be disabled
+            textBoxCaseEntry.Enabled = true;
+            buttonEnter.Enabled = true;
+
+            // Reset the values of the class-level variables
+            currentDay = 1;
+            totalCases = 0;
+
+            // Set displayed day back to default
+            labelDay.Text = "Day" + currentDay;
+
+            // Set the focus
+            textBoxCaseEntry.Focus();
+
+        }
+        /// <summary>
+        /// Validates and accepts the value for the number of cases entered by the user. When it is valid,  
+        /// records the value and adds it to a running total. Once all the values for 7 days are entered,
+        /// calculates and displays the average of those values.
+        /// </summary>
+        private void EnterClick(object sender, EventArgs e)
+        {
+            // Declare a constant representing the number of days
+            const int  NumberOfDays = 7;
+            // Declare a variable for storing the user's last numeric input
+            int enteredCases;
+
+            // If the contents of the textbox are a whole number...
+            if (int.TryParse(textBoxCaseEntry.Text, out enteredCases))
+            {
+                // If the contents are within a valid range...
+                if (enteredCases >= 0 && enteredCases <= int.MaxValue)
+                {
+                    // Increment number of cases based on user input
+                    totalCases += enteredCases;
+                    // Add the last entered value to large textbox
+                    textBoxCaseList.Text += enteredCases + Environment.NewLine;
+                    // Increment the day
+                    currentDay++;
+
+                    // If the day is greater than 7...
+                    if (currentDay > NumberOfDays)
+                    {
+                        // Set the day equal to 7
+                        currentDay = 7;
+
+                        // Calculate average
+                        double averageCases = (double)totalCases / NumberOfDays;
+
+                        // Display the average
+                        labelOutput.Text = "Average cases per day: " + Math.Round(averageCases, 2);
+
+                        // Disable input-related controls
+                        textBoxCaseEntry.Enabled = false;
+                        buttonEnter.Enabled = false;
+                    }
+
+                    // Update the label that displays the current day.
+                    labelDay.Text = "Day " + currentDay;
+
+                    // Set the focus to the input textbox.
+                    textBoxCaseEntry.Clear();
+                    textBoxCaseEntry.Focus();
+                }
+                // Contents not in range. Report error.
+                else
+                {
+                    MessageBox.Show("Your entry must be between 0 and " + int.MaxValue, "Entry Error!!!");
+                    textBoxCaseEntry.SelectAll();
+                    textBoxCaseEntry.Focus();
+                }
+
+             }
+             // The contents are not a whole number. Report an error.
+             else
+              {
+                  MessageBox.Show("Your entry must be a whole number.", "Entry Error!!!");
+                  textBoxCaseEntry.SelectAll();
+                  textBoxCaseEntry.Focus();
+              }
+            }
+
+        private void formAverageCases_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
